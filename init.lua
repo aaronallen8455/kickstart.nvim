efdd263ab4d5532cb3871f9f90327c55138a17c4
Aaron Allen <aaronallen8455@gmail.com>
2025-12-04 22:50:58 -0600

    fix linter

--- init.lua
+++ init.lua
@@ -175,9 +175,9 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
-  'dense-analysis/ale',
   'scrooloose/nerdtree',
   'PhilRunninger/nerdtree-visual-selection',
+  'kien/ctrlp.vim',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
